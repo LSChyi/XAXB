cc5cf4a4dd5448a79fde0d81a09417273f6be497
LSChyi <alan81920@gmail.com>
2016-04-23 10:01:49 +0800

    first commit

--- XAXB/main.swift
+++ XAXB/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-print("Hello, World!")
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let rawString = NSString(data: inputData, encoding:NSUTF8StringEncoding)
+    if let string = rawString {
+        return string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    } else {
+        return "Invalid input"
+    }
+}
 
+func getAnswer() -> (Int, Int) {
+    print("Enter number of As:")
+    let a = input()
+    print("Enter number of Bs:")
+    let b = input()
+    
+    return (Int(a)!, Int(b)!)
+}
+
+class AnswerMgr {
+    var answers = [Answer]()
+    init() {
+        // initialize all possible answers
+    }
+}
+
+class Answer {
+    var answer = [Int]()
+    init(answer: [Int]) {
+        self.answer = answer
+    }
+    func check(numberOfA: Int, numberOfB: Int, answer: [Int]) -> Bool {
+        // check is number of As and Bs are the same
+        return true
+    }
+}
